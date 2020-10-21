1b5a243b95c161308d011bfedff5c28d49577eb3
Dennis Desktop <dwichelo@gmail.com>
2020-10-21 16:09:31 +0200

    Added Angular

---
+++ src/main/HelloWorldDeluxe/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.sass']
+})
+
+export class AppComponent implements OnInit {
+  title = 'HelloWorldDeluxe';
+
+  ngOnInit(): void {
+    console.log('Angular has been loaded and has served the correct HTML page');
+  }
+
+}
