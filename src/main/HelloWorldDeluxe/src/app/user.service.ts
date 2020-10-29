5683755ee7681779ed17a69e191e80446ca58a6f
Dennis Laptop <dwichelo@gmail.com>
2020-10-29 12:45:28 +0100

    Added actual view for the user list

---
+++ src/main/HelloWorldDeluxe/src/app/user.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import {User} from "./user";
+import {Observable, of} from "rxjs";
+import {catchError, tap} from "rxjs/operators";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor(private http: HttpClient) {
+  }
+
+  //The 'proxy' part of the URL is resolved by the proxy.conf.json file when starting the server in developer mode (using npm start)
+  //this may need to be changed when the app is running in production mode
+  private url = "proxy/registration/users";
+
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
+
+
+  getUsers(): Observable<User[]> {
+    return this.http.get<User[]>(this.url).pipe(
+        catchError(this.handleError<User[]>('getUsers', []))
+      )
+  }
+
+  addUser(user: User): Observable<User> {
+    return this.http.post<User>(this.url, user, this.httpOptions).pipe(
+      catchError(this.handleError<User>('addUser'))
+    )
+  }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error);
+      return of(result as T);
+    };
+  }
+
+}
